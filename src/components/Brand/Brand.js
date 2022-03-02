765414f5e29b5c3c9d9e06bdeb8e763bd2cc93ea
vahidrn98 <v1990mail@gmail.com>
2022-03-02 13:41:34 +0330

    Header and brands section

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,9 +1,14 @@
 import "./Brand.css";
+import {brands} from "./images";
 
 function Brand() {
- return (
-<div>
 
-</div>
-);}
+  return (
+    <div className="gpt3__brand section__padding">
+      {brands.map((brand) => (
+        <img src={brand} alt="google" />
+      ))}
+    </div>
+  );
+}
 export default Brand;
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,7 +1,41 @@
 import "./Header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 function Header() {
- return (
- <div>Header</div>
-);}
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Lets Build Something amazing with GPT-3 and OpenAI
+        </h1>
+        <p className="gpt3__header-content__description">
+          lorem sajjdfnjdbfjnnf jb jdfjdbjndfbs hdbfbja bdfdfjkdb fbbdndnf
+          jndfjnd fjndfndnffndnf nb kj ndf anbfbdbfbd bfbdbb b bjbf bsdhbwlke
+          kjewnj bkjb hjbf kjbdahfb jkf
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Submit</button>
+        </div>
+        <div className="gpt3__header-content__users">
+          <div className="gpt3__header-content__users-images">
+            <img
+              src={people}
+              alt="user"
+              className="gpt3__header-content__users-avatar"
+            />
+          </div>
+          
+            <p>160,000 people are currently using our products.</p>
+          
+        </div>
+        
+      </div>
+      <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+    </div>
+  );
+}
 export default Header;
