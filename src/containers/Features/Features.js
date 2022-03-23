32a106185340ba582457236dc262caa9fdfedd79
vahidrn98 <v1990mail@gmail.com>
2022-03-23 21:06:14 +0430

    features section added

--- src/containers/Features/Features.js
+++ src/containers/Features/Features.js
@@ -1,7 +1,47 @@
 import "./Features.css";
+import Feature from "../../components/Feature/Feature";
 
 function Features() {
- return (
- <div></div>
-);}
+  const featuresData = [
+    {
+      title: "something cool.",
+      description:
+        "the description for the feature the description for the feature the description for the feature the description for the feature the description for the feature",
+    },
+    {
+      title: "something cool.",
+      description:
+        "the description for the feature the description for the feature the description for the feature the description for the feature the description for the feature",
+    },
+    {
+      title: "something cool.",
+      description:
+        "the description for the feature the description for the feature the description for the feature the description for the feature the description for the feature",
+    },
+    {
+      title: "something cool.",
+      description:
+        "the description for the feature the description for the feature the description for the feature the description for the feature the description for the feature",
+    },
+  ];
+
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The future is now and you just have to realize it. Step into future
+          today and make it happen.
+        </h1>
+        <p>Request early access to get started.</p>
+      </div>
+      <div className="gpt3__features-container">
+        {
+            featuresData.map((item,index) => (
+          <Feature title={item.title} text={item.description} key={index}/>
+        ))
+    }
+      </div>
+    </div>
+  );
+}
 export default Features;
