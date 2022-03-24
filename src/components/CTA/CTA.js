ff6364e88fbfb50732ab1aa790feb5329b30172c
vahidrn98 <v1990mail@gmail.com>
2022-03-24 20:14:10 +0430

    CTA start

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -1,7 +1,16 @@
 import "./CTA.css";
 
 function CTA() {
- return (
- <div></div>
-);}
+  return (
+    <div className="gpt3__cta" id="cta">
+      <div className="gpt3__cta-content">
+        <p>Request early access to get started.</p>
+        <h3>Register today and start exploring the endless possibility</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+}
 export default CTA;
